a2c94fa03b2e80659eee221489a94d8f4dc400d5
Ruben Deisenroth <rdeisenroth@gmail.com>
2022-07-14 17:54:46 +0200

    Refactor EnemyMovement

--- src/main/java/h13/model/EnemyMovement.java
+++ src/main/java/h13/model/EnemyMovement.java
@@ -47,46 +47,48 @@ public class EnemyMovement implements Playable {
             final double progress = movementProgress.get() + elapsedTime / (verticalMovement.get() ? VERTICAL_ENEMY_MOVEMENT_DURATION : HORIZONTAL_ENEMY_MOVEMENT_DURATION);
             movementProgress.set(progress);
             if (movementProgress.get() >= 1) {
-                movementProgress.set(0);
-                if (verticalMovement.get()) {
-                    horizontalMovementDirection.set(horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? HorizontalDirection.RIGHT : HorizontalDirection.LEFT);
-                } else {
-                    verticalMovementIteration.set(verticalMovementIteration.get() + 1);
-                }
-                verticalMovement.set(!verticalMovement.get());
+                nextMovement();
             } else {
-//                var insets = getEnemyController().getGameBoard().getBorder().getInsets();
-//                var horizontalSpace = getEnemyController().getGameBoard().getMaxWidth() - insets.getLeft() - insets.getRight();
-//                var verticalSpace = getEnemyController().getGameBoard().getMaxHeight() - insets.getTop() - insets.getBottom();
-                final var horizontalSpace = getEnemyController().getGameBoard().getWidth();
-                final var verticalSpace = getEnemyController().getGameBoard().getHeight();
-                final var horizontalEnemySpace = horizontalSpace * (1 - HORIZONTAL_ENEMY_MOVE_DISTANCE);
-                final var chunkSize = horizontalEnemySpace / ENEMY_COLS;
-                final var padding = chunkSize / 2 - GameConstants.RELATIVE_SHIP_WIDTH * horizontalSpace / 2;
-                for (final Enemy enemy : getEnemyController().getAliveEnemies()) {
-                    final var enemyXPos = chunkSize * enemy.getxIndex() + +padding;
-                    final var enemyYPos = chunkSize * enemy.getyIndex() + +padding;
-                    if (verticalMovement.get()) {
-                        enemy.setX(enemyXPos + (horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? 0 : (HORIZONTAL_ENEMY_MOVE_DISTANCE * horizontalSpace)));
-                        enemy.setY(enemyYPos + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * (verticalMovementIteration.get() - 1) + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * movementProgress.get());
-                    } else {
-                        enemy.setX(enemyXPos + (horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? 1 - progress : progress) * (HORIZONTAL_ENEMY_MOVE_DISTANCE * horizontalSpace));
-                        enemy.setY(enemyYPos + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * verticalMovementIteration.get());
-                    }
-                }
+                updatePositions();
             }
         }
         lastUpdate.set(now);
     }
 
+    private void updatePositions() {
+        final var horizontalSpace = getEnemyController().getGameBoard().getWidth();
+        final var verticalSpace = getEnemyController().getGameBoard().getHeight();
+        final var horizontalEnemySpace = horizontalSpace * (1 - HORIZONTAL_ENEMY_MOVE_DISTANCE);
+        final var chunkSize = horizontalEnemySpace / ENEMY_COLS;
+        final var padding = chunkSize / 2 - GameConstants.RELATIVE_SHIP_WIDTH * horizontalSpace / 2;
+        for (final Enemy enemy : getEnemyController().getAliveEnemies()) {
+            final var enemyXPos = chunkSize * enemy.getxIndex() + +padding;
+            final var enemyYPos = chunkSize * enemy.getyIndex() + +padding;
+            if (verticalMovement.get()) {
+                enemy.setX(enemyXPos + (horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? 0 : (HORIZONTAL_ENEMY_MOVE_DISTANCE * horizontalSpace)));
+                enemy.setY(enemyYPos + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * (verticalMovementIteration.get() - 1) + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * movementProgress.get());
+            } else {
+                enemy.setX(enemyXPos + (horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? 1 - getMovementProgress() : getMovementProgress()) * (HORIZONTAL_ENEMY_MOVE_DISTANCE * horizontalSpace));
+                enemy.setY(enemyYPos + (VERTICAL_ENEMY_MOVE_DISTANCE * verticalSpace) * verticalMovementIteration.get());
+            }
+        }
+    }
+
+    private void nextMovement() {
+        movementProgress.set(0);
+        if (verticalMovement.get()) {
+            horizontalMovementDirection.set(horizontalMovementDirection.get().equals(HorizontalDirection.LEFT) ? HorizontalDirection.RIGHT : HorizontalDirection.LEFT);
+        } else {
+            verticalMovementIteration.set(verticalMovementIteration.get() + 1);
+        }
+        verticalMovement.set(!verticalMovement.get());
+    }
+
     public boolean bottomWasReached() {
-//        for (Enemy enemy : getEnemyController().getAliveEnemies()) {
-//            if (enemy.getY() + enemy.getHeight() > getEnemyController().getGameBoard().getHeight()) {
-//                return true;
-//            }
-//        }
-//        return false;
-        return getEnemyController().getAliveEnemies().stream().anyMatch(enemy -> enemy.getY() + enemy.getHeight() > getEnemyController().getGameBoard().getHeight());
+        return getEnemyController()
+            .getAliveEnemies()
+            .stream()
+            .anyMatch(enemy -> enemy.getY() + enemy.getHeight() > getEnemyController().getGameBoard().getHeight());
     }
 
     public EnemyController getEnemyController() {
