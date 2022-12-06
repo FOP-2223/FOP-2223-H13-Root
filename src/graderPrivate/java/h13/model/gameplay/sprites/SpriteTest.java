7b21499cf136b39e6b6b800bb8273cc06aab48df
oxolotel <94632508+oxlotel21@users.noreply.github.com>
2022-12-06 12:24:53 +0100

    add Sprite.update() Tests

--- src/graderPrivate/java/h13/model/gameplay/sprites/SpriteTest.java
+++ src/graderPrivate/java/h13/model/gameplay/sprites/SpriteTest.java
@@ -1,16 +1,33 @@
 package h13.model.gameplay.sprites;
 
+import h13.controller.GameConstants;
+import h13.model.gameplay.Direction;
+import h13.shared.Utils;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockSettings;
 import org.mockito.Mockito;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.*;
+import static spoon.testing.Assert.assertThat;
 
 @TestForSubmission
 public class SpriteTest {
@@ -71,8 +88,102 @@ public class SpriteTest {
     }
 
     @Test
-    void update() {
+    void update_inside() {
+        GameConstants.ORIGINAL_GAME_BOUNDS = new BoundingBox(0, 0, 100, 100);
+        Bounds destination = new BoundingBox(50, 50, 1, 1);
 
+        Context context = contextBuilder()
+            .add("Game Bounds", GameConstants.ORIGINAL_GAME_BOUNDS)
+            .add("Next Position", destination)
+            .build();
+
+        try (var utilsMock = mockStatic(Utils.class)){
+            utilsMock.when(() -> Utils.getNextPosition(
+                    any(Bounds.class),
+                    anyDouble(),
+                    any(),
+                    anyDouble()
+                ))
+                .thenReturn(destination);
+            utilsMock.when(() -> Utils.clamp(
+                    any(Bounds.class)
+                ))
+                .thenReturn(destination);
+
+            Sprite sprite = createSprite(1);
+            sprite.update(0);
+
+            utilsMock.verify(() -> Utils.getNextPosition(
+                any(Bounds.class),
+                anyDouble(),
+                any(),
+                anyDouble()
+            ), atLeastOnce());
+
+            ArgumentCaptor<Double> argumentSetX = ArgumentCaptor.forClass(Double.class);
+            verify(sprite).setX(argumentSetX.capture());
+
+            ArgumentCaptor<Double> argumentSetY = ArgumentCaptor.forClass(Double.class);
+            verify(sprite).setX(argumentSetY.capture());
+
+            assertTrue(argumentSetX.getValue() < GameConstants.ORIGINAL_GAME_BOUNDS.getWidth(), context, r -> String.format(""));
+            assertTrue(argumentSetY.getValue() < GameConstants.ORIGINAL_GAME_BOUNDS.getHeight(), context, r -> String.format(""));
+
+            assertEquals(destination.getMinX(), sprite.getX(), context, r-> String.format(""));
+            assertEquals(destination.getMinY(), sprite.getY(), context, r-> String.format(""));
+        }
+    }
+
+    @Test
+    void update_outside() {
+        GameConstants.ORIGINAL_GAME_BOUNDS = new BoundingBox(0, 0, 100, 100);
+        Bounds destination = new BoundingBox(500, 500, 1, 1);
+        Bounds clampedDestination = new BoundingBox(50, 50, 1, 1);
+
+        Context context = contextBuilder()
+            .add("Game Bounds", GameConstants.ORIGINAL_GAME_BOUNDS)
+            .add("Next Position", destination)
+            .build();
+
+        try (var utilsMock = mockStatic(Utils.class)){
+            utilsMock.when(() -> Utils.getNextPosition(
+                    any(Bounds.class),
+                    anyDouble(),
+                    any(),
+                    anyDouble()
+                ))
+                .thenReturn(destination);
+            utilsMock.when(() -> Utils.clamp(
+                    any(Bounds.class)
+                ))
+                .thenReturn(new BoundingBox(50, 50, 1, 1));
+
+            Sprite sprite = createSprite(1);
+            sprite.update(0);
+
+            utilsMock.verify(() -> Utils.getNextPosition(
+                any(Bounds.class),
+                anyDouble(),
+                any(),
+                anyDouble()
+            ), atLeastOnce());
+
+            utilsMock.verify(() -> Utils.clamp(
+                any(Bounds.class)
+            ), atLeastOnce());
+
+            ArgumentCaptor<Double> argumentSetX = ArgumentCaptor.forClass(Double.class);
+            verify(sprite).setX(argumentSetX.capture());
+
+            ArgumentCaptor<Double> argumentSetY = ArgumentCaptor.forClass(Double.class);
+            verify(sprite).setX(argumentSetY.capture());
+
+            assertTrue(argumentSetX.getAllValues().stream().allMatch(d -> d < GameConstants.ORIGINAL_GAME_BOUNDS.getWidth()), context, r -> "SetX was called with out of bounds coordinates");
+            assertTrue(argumentSetY.getAllValues().stream().allMatch(d -> d < GameConstants.ORIGINAL_GAME_BOUNDS.getHeight()), context, r -> "SetY was called with out of bounds coordinates");
+
+            assertEquals(clampedDestination.getMinX(), sprite.getX(), context, r-> String.format(""));
+            assertEquals(clampedDestination.getMinY(), sprite.getY(), context, r-> String.format(""));
+        }
     }
 
     private static Sprite createSprite(int health){
