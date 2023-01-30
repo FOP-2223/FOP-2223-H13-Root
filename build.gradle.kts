0b65cd2547b5aa0c7286baa3b66582d621816a01
Ruben Deisenroth <rdeisenroth@gmail.com>
2023-01-30 12:39:01 +0100

    cleanup + jagr 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.sourcegrade.jagr.launcher.env.Config
+import org.sourcegrade.jagr.launcher.env.Executor
+
 @Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     java
@@ -16,6 +19,9 @@ javafx {
 
 jagr {
     assignmentId.set("h13")
+    download {
+        jagrVersion.set("0.8.0")
+    }
     submissions {
         val main by creating {
             studentId.set("ab12cdef")
@@ -27,6 +33,12 @@ jagr {
         val graderPublic by creating {
             graderName.set("FOP-2223-H13-Public")
             rubricProviderName.set("h13.H13_RubricProvider")
+            config.set(Config(
+                executor = Executor(jvmArgs = listOf("-Dprism.order=sw")),
+                transformers = org.sourcegrade.jagr.launcher.env.Transformers(
+                    timeout = org.sourcegrade.jagr.launcher.env.Transformers.TimeoutTransformer(enabled = false)
+                )
+            ))
             configureDependencies {
                 implementation(libs.algoutils.tutor)
                 implementation(libs.mockito.inline)
@@ -37,7 +49,6 @@ jagr {
                 implementation(libs.jackson.databind)
                 compileOnly(libs.openjfx.monocle)
             }
-            disableTimeouts()
         }
         val graderPrivate by creating {
             parent(graderPublic)
