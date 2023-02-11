aa65eb6b547f19188f64e1a3e60f7ae1b73651b4
Rdeisenroth <53945743+Rdeisenroth@users.noreply.github.com>
2023-02-11 16:40:22 +0100

    private tests Ruben (#11)
    * update Private Tests with improvements from public-Tests
    
    * update build.gradle.kts
    
    * update h3 tests

--- src/graderPrivate/java/h13/model/gameplay/sprites/EnemyTest.java
+++ src/graderPrivate/java/h13/model/gameplay/sprites/EnemyTest.java
@@ -3,11 +3,15 @@ package h13.model.gameplay.sprites;
 import h13.controller.ApplicationSettings;
 import h13.model.gameplay.Direction;
 import h13.model.gameplay.GameState;
+import h13.util.PrettyPrinter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 
+import static h13.util.StudentLinks.BattleShipLinks.BattleShipMethodLink.SHOOT_METHOD;
+import static h13.util.StudentLinks.EnemyLinks.EnemyMethodLink.UPDATE_METHOD;
 import static org.mockito.Mockito.*;
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.contextBuilder;
 
 @TestForSubmission
 public class EnemyTest {
@@ -16,24 +20,39 @@ public class EnemyTest {
     @BeforeEach
     public void setup() {
         ApplicationSettings.loadTexturesProperty().set(false);
+        ApplicationSettings.enemyShootingDelayProperty().set(0);
         // replace Math.random() calls in Enemy with MathRandomTester.random() calls
-        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
     }
 
     @Test
     public void testUpdateShootCalledWithMaxProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(1);
-        ApplicationSettings.enemyShootingDelayProperty().set(0);
-        enemy.update(10);
-        verify(enemy, times(1)).shoot(Direction.DOWN);
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
+        final var context = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 10)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        UPDATE_METHOD.invoke(context, enemy, 10);
+        SHOOT_METHOD.assertInvokedNTimesWithParams(context, enemy, 1, Direction.DOWN);
     }
 
     @Test
     public void testUpdateWithMinProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(0);
-        ApplicationSettings.enemyShootingDelayProperty().set(0);
-        enemy.update(0);
-        verify(enemy, never()).shoot(Direction.DOWN);
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
+        final var context = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+
+        UPDATE_METHOD.invoke(context, enemy, 0);
+        verify(enemy, never().description(context.toString())).shoot(any());
     }
 
     @Test
@@ -41,30 +60,69 @@ public class EnemyTest {
         ApplicationSettings.enemyShootingProbabilityProperty().set(1);
         ApplicationSettings.enemyShootingDelayProperty().set(1000);
         enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
-        enemy.update(0);
-        verify(enemy, never()).shoot(Direction.DOWN);
-        enemy.update(1000);
-        verify(enemy, times(1)).shoot(Direction.DOWN);
+        final var context = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        UPDATE_METHOD.invoke(context, enemy, 0);
+        verify(enemy, never().description(context.toString())).shoot(Direction.DOWN);
+        final var context2 = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 1000)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        UPDATE_METHOD.invoke(context2, enemy, 1000);
+        verify(enemy, times(1).description(context2.toString())).shoot(Direction.DOWN);
     }
 
     @Test
     public void testUpdateWithDelayAndMinProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(0);
         ApplicationSettings.enemyShootingDelayProperty().set(1000);
-        enemy.update(0);
-        verify(enemy, never()).shoot(Direction.DOWN);
-        enemy.update(1000);
-        verify(enemy, never()).shoot(Direction.DOWN);
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
+        final var context = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        UPDATE_METHOD.invoke(context, enemy, 0);
+        verify(enemy, never().description(context.toString())).shoot(Direction.DOWN);
+        final var context2 = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 1000)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        UPDATE_METHOD.invoke(context2, enemy, 1000);
+        verify(enemy, never().description(context2.toString())).shoot(Direction.DOWN);
     }
 
     @Test
     public void testUpdateWithFiftyPercentProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(0.5);
         ApplicationSettings.enemyShootingDelayProperty().set(0);
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
+        final var context = contextBuilder()
+            .add("enemy", PrettyPrinter.prettyPrint(enemy))
+            .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
+            .add("shootingDelay", ApplicationSettings.enemyShootingDelayProperty().get())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .add("note:", "this test case tests 1000 iterations of the update method, and is therefore not deterministic."
+                + "If you are certain that your implementation is correct but you still get a failure, write a complaint")
+            .build();
         for (double i = 0; i <= 1; i+=0.001) {
             enemy.update(1000);
         }
-        verify(enemy, atLeast(1)).shoot(Direction.DOWN);
-        verify(enemy, atMost(999)).shoot(Direction.DOWN);
+        verify(enemy, atLeast(1).description(context.toString())).shoot(Direction.DOWN);
+        verify(enemy, atMost(999).description(context.toString())).shoot(Direction.DOWN);
     }
 }
--- src/graderPrivate/java/h13/model/gameplay/sprites/PlayerTest.java
+++ src/graderPrivate/java/h13/model/gameplay/sprites/PlayerTest.java
@@ -1,16 +1,23 @@
 package h13.model.gameplay.sprites;
 
 import h13.controller.ApplicationSettings;
+import h13.model.gameplay.Direction;
 import h13.model.gameplay.GameState;
+import h13.util.PrettyPrinter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 
+import static h13.util.StudentLinks.BattleShipLinks.BattleShipMethodLink.SHOOT_METHOD;
+import static h13.util.StudentLinks.PlayerLinks.PlayerMethodLink.UPDATE_METHOD;
 import static org.mockito.Mockito.*;
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.contextBuilder;
 
 @TestForSubmission
 public class PlayerTest {
     private Player player;
+
     @BeforeEach
     public void setup() {
         ApplicationSettings.loadTexturesProperty().set(false);
@@ -20,14 +27,31 @@ public class PlayerTest {
     @Test
     public void testUpdateWithKeepShooting() {
         player.setKeepShooting(true);
-        player.update(0);
-        verify(player, times(1)).shoot();
+        final var context = contextBuilder()
+            .add("player", PrettyPrinter.prettyPrint(player))
+            .add("keepShooting", player.isKeepShooting())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+        SHOOT_METHOD.alwaysDoNothing(context, player);
+
+        UPDATE_METHOD.invoke(context, player, 0);
+        SHOOT_METHOD.assertInvokedNTimesWithParams(context, player, 1, Direction.UP);
     }
 
     @Test
     public void testUpdateWithoutKeepShooting() {
         player.setKeepShooting(false);
-        player.update(0);
-        verify(player, never()).shoot();
+        final var context = contextBuilder()
+            .add("player", PrettyPrinter.prettyPrint(player))
+            .add("keepShooting", player.isKeepShooting())
+            .add("elapsedTime", 0)
+            .add("requires other methods to work:", "super.update(elapsedTime)")
+            .build();
+
+        SHOOT_METHOD.alwaysDoNothing(context, player);
+
+        UPDATE_METHOD.invoke(context, player, 0);
+        SHOOT_METHOD.invokeWithoutCatching(context, verify(player, Mockito.never().description(context.toString())), any(Direction.class));
     }
 }
